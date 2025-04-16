ac263cbcf10327d716be309e4feb1b32b80b55a4
Suryansh Pal <defnvary@gmail.com>
2025-04-16 18:46:20 +0000

    used copilot to add a new section and modify some css rules

---
+++ src/Components/Education.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div className="education">
+      <h2>Education</h2>
+      <div className="education-item">
+        <h3>Bachelor of Technology in Computer Science</h3>
+        <p>PIEMR, Indore, 2023 - Present</p>
+        <ul>
+          <li>Relevant Coursework: Data Structures, Algorithms, OOPM</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Education;
